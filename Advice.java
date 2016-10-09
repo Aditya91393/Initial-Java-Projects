f166d2ee67601776b0b2982931dd7eb970c9f47e
Aditya Sharma <narutosage9@hotmail.ca>
2016-10-09 16:49:09 -0400

    Add files via upload

---
+++ Advice.java
@@ -0,0 +1,16 @@
+public class Advice
+{
+ public static void main (String[] args)
+ {
+ System.out.print ("If at first ");
+ System.out.println ("you don't succeed" + ", failure ");
+ System.out.print ("is not ");
+ System.out.println ("an option.");
+ 
+ /* "System.out.print" and "System.out.println" are completely different
+ The "ln" tells the compiler to create a spacing between that line and the next.
+ So basically the next line is actually 2 lines away.*/
+ 
+ // Here I learnt that we could add words.
+}
+}
