90a06b676a413f27b8127fbd6d1f37c0d688fe0e
Aditya Sharma <narutosage9@hotmail.ca>
2016-10-09 16:50:21 -0400

    Add files via upload

---
+++ DeclareAssignString.java
@@ -0,0 +1,24 @@
+public class DeclareAssignString
+{
+public static void main (String[] args)  // end of line comment
+{
+    
+int numberOne = 8;  // this is declaring an int variable and assigning a value all in one statement
+double numberTwo;  // this is declaring a double variable
+String name;  // this is declaring a String
+    
+name = "Dr. Aditya Sharma"; // this is assigning a String value to the variable
+numberTwo = 95; // this is assigning a double value to the variable
+    
+      
+System.out.println ("The name variable holds " + name);
+System.out.println ("The int variable holds " + numberOne);
+System.out.println ("The double variable holds " + numberTwo);
+    
+   
+System.out.println ("\n" + name + " is the best doctor in North America.");
+System.out.println (name + " had extremely high marks in University and Highschool." + " He had an average of " + numberTwo);
+    
+    
+} // end main method
+} // end class
