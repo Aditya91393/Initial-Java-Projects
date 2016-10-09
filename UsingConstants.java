d4c7f84c2b47ab216335dfa42746b93e71a00e43
Aditya Sharma <narutosage9@hotmail.ca>
2016-10-09 16:54:01 -0400

    Add files via upload

---
+++ UsingConstants.java
@@ -0,0 +1,43 @@
+//**********************************************************
+// Assignment: (JxAy Title of Assignment here)
+//
+// Description: (Enter a brief description of what the program does here)
+//
+// Author: (Enter your full name here)
+//
+// Date Start: (Enter date started)
+// Date Completed: (Enter date completed)
+//
+// Completion time: (Enter the total number of hours or
+//              periods you spent on the assignment)
+//
+// Honor Code: I pledge that this program represents my own
+//   program code. I received help from (enter the names of
+//   others that helped with the assignment; write no one if
+//   you received no help) in designing and debugging my program.
+//*********************************************************
+
+
+   public class UsingConstants
+   {
+      public static void main (String[] args)  // end of line comment
+      {
+      
+      // Step 1 - Declaring variables
+         int originalNumber = 5;
+         //The following line declares a CONSTANT value for a variable
+         final int finalNumber = 5;
+      
+      // Step 2 - (Input) Assigning values to variables
+         
+         
+      
+    
+
+      
+      // Step 4 - Output
+         System.out.println ("the canChangeNumber is " + originalNumber);
+         System.out.println ("the constant CANNOT_CHANGE_NUMBER is " + finalNumber);
+      
+      } // end main method
+   } // end class
